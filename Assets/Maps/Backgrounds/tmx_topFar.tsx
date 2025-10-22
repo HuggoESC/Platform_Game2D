60c913aa0241defd016a0a5fd1ca4eac77acdf62
HuggESC <hugo.escobar@estudiantat.upc.edu>
2025-10-22 23:06:49 +0200

    rayada
    vuelta al inicio

--- Assets/Maps/Backgrounds/tmx_topFar.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="tmx_topFar" tilewidth="32" tileheight="32" tilecount="640" columns="20">
- <image source="top_far_bgrnd.png" width="640" height="1024"/>
-</tileset>
