686972975360bc1fe6444d1c23ce9af0e142b4b0
HuggESC <hugo.escobar@estudiantat.upc.edu>
2025-10-22 23:30:35 +0200

    me quiero matar

---
+++ Assets/Maps/Tilesets/Ground.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Ground" tilewidth="32" tileheight="32" tilecount="165" columns="15">
+ <image source="../oak_woods_v1.0/oak_woods_tileset.png" width="504" height="360"/>
+</tileset>
