b89c86c7a592f65368dbfe00f5847a9b1f4aaebe
HuggESC <hugo.escobar@estudiantat.upc.edu>
2025-10-23 09:52:10 +0200

    Add knight character textures and animations
    Added PNG spritesheets and GIF animations for the 'Descarga1Caballero' knight character in two color variants, with both outlined and non-outlined versions. Also included a comparison image, promo animation, and supporting files for the knight character.

---
+++ Assets/Textures/Characters/Knight.tsx
@@ -0,0 +1,39 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Knight" tilewidth="120" tileheight="80" tilecount="30" columns="10">
+ <image source="Knight_Spritesheet.png" width="1200" height="240"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="5" duration="100"/>
+   <frame tileid="6" duration="100"/>
+   <frame tileid="7" duration="100"/>
+   <frame tileid="8" duration="100"/>
+   <frame tileid="9" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="10">
+  <animation>
+   <frame tileid="10" duration="50"/>
+   <frame tileid="11" duration="50"/>
+   <frame tileid="12" duration="50"/>
+   <frame tileid="13" duration="50"/>
+   <frame tileid="14" duration="50"/>
+   <frame tileid="15" duration="50"/>
+   <frame tileid="16" duration="50"/>
+   <frame tileid="17" duration="50"/>
+   <frame tileid="18" duration="50"/>
+   <frame tileid="19" duration="50"/>
+  </animation>
+ </tile>
+ <tile id="20">
+  <animation>
+   <frame tileid="20" duration="50"/>
+   <frame tileid="21" duration="50"/>
+   <frame tileid="22" duration="50"/>
+  </animation>
+ </tile>
+</tileset>
