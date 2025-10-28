75a0d391567b8a7951df93baa8900ccf42e5bbac
HuggESC <hugo.escobar@estudiantat.upc.edu>
2025-10-28 18:48:23 +0100

    nose
    nose

---
+++ Assets/Maps/1-Player-Bomb Guy.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="1-Player-Bomb Guy" tilewidth="32" tileheight="32" tilecount="1" columns="1">
+ <image source="../Textures/Pirate Bomb/Aseprite Files/1-Player-Bomb Guy.aseprite" width="58" height="58"/>
+</tileset>
