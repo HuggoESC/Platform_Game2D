22686b9856efb1bd5cade932299f2b1c4c6a0782
HuggESC <hugo.escobar@estudiantat.upc.edu>
2025-11-24 23:15:59 +0100

    Added Enemy on tiled

---
+++ Assets/Maps/Tilesets/slime.tsx
@@ -0,0 +1,48 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="slime" tilewidth="32" tileheight="32" tilecount="120" columns="12">
+ <image source="../../Textures/2.0slime.png" width="384" height="320"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="150"/>
+   <frame tileid="1" duration="150"/>
+   <frame tileid="2" duration="150"/>
+   <frame tileid="3" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="4">
+  <animation>
+   <frame tileid="3" duration="150"/>
+   <frame tileid="4" duration="150"/>
+   <frame tileid="12" duration="150"/>
+   <frame tileid="13" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="14">
+  <animation>
+   <frame tileid="14" duration="150"/>
+   <frame tileid="15" duration="150"/>
+   <frame tileid="16" duration="150"/>
+   <frame tileid="24" duration="150"/>
+   <frame tileid="25" duration="150"/>
+   <frame tileid="26" duration="150"/>
+   <frame tileid="27" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="28">
+  <animation>
+   <frame tileid="27" duration="150"/>
+   <frame tileid="28" duration="150"/>
+   <frame tileid="36" duration="150"/>
+   <frame tileid="37" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="38">
+  <animation>
+   <frame tileid="38" duration="150"/>
+   <frame tileid="39" duration="150"/>
+   <frame tileid="40" duration="150"/>
+   <frame tileid="48" duration="150"/>
+   <frame tileid="49" duration="150"/>
+  </animation>
+ </tile>
+</tileset>
