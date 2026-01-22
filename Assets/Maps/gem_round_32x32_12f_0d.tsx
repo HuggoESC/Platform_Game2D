2c78895a8ddc7b936198479130cce6863a98d0a6
HuggESC <hugo.escobar@estudiantat.upc.edu>
2026-01-22 20:05:31 +0100

    Refactor asset structure and update gem assets
    Moved various image and audio assets from 'Assets/Final/Final' and 'Assets/Final/Final/Social' to 'Assets/Maps' for better organization. Removed old coin and gem images, added new gem animation assets and updated map references accordingly. Also replaced the coin pickup sound effect and updated Level01.tmx to include a new Gem object.

---
+++ Assets/Maps/gem_round_32x32_12f_0d.tsx
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="gem_round_32x32_12f_0d" tilewidth="32" tileheight="32" tilecount="12" columns="12">
+ <image source="../Textures/gem_round_32x32_12f_0d.png" width="384" height="32"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="180"/>
+   <frame tileid="1" duration="180"/>
+   <frame tileid="2" duration="180"/>
+   <frame tileid="3" duration="180"/>
+   <frame tileid="4" duration="180"/>
+   <frame tileid="5" duration="180"/>
+   <frame tileid="6" duration="180"/>
+   <frame tileid="7" duration="180"/>
+   <frame tileid="8" duration="180"/>
+   <frame tileid="9" duration="180"/>
+   <frame tileid="10" duration="180"/>
+   <frame tileid="11" duration="180"/>
+  </animation>
+ </tile>
+</tileset>
---
+++ Assets/Maps/pick_up_effect.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="pick_up_effect" tilewidth="32" tileheight="32" tilecount="100" columns="10">
+ <image source="../Coins/pick_up_effect.gif" width="320" height="320"/>
+</tileset>
